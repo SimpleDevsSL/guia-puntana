963d505dadcad236b3f87386815970dcdaeb87d2
JuanSr02 <juanma2002123@gmail.com>
2026-01-14 09:21:09 -0300

    Mejores mensajes de error

--- components/auth/auth-schema.ts
+++ components/auth/auth-schema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const authSchema = z.object({
-  email: z.string().email({ message: 'Correo electrónico inválido.' }),
-  password: z.string().min(8, { message: 'Mínimo 8 caracteres.' }),
+  email: z.string().email({ message: 'El correo electrónico no es válido.' }),
+  password: z.string().min(8, { message: 'La contraseña debe tener al menos 8 caracteres.' }),
 });
 
 export type AuthFormData = z.infer<typeof authSchema>;
