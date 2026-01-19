0cfb0437152fd56b2ac61e6bb9383f2cda20c4ee
JuanSr02 <juanma2002123@gmail.com>
2026-01-18 21:09:31 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
-import prettierConfig from 'eslint-config-prettier'; // Importar
-import prettierPlugin from 'eslint-plugin-prettier'; // Importar
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -12,12 +12,15 @@ const eslintConfig = defineConfig([
       prettier: prettierPlugin,
     },
     rules: {
-      'prettier/prettier': 'error', // Muestra errores de formato como errores de ESLint
+      'prettier/prettier': 'warn',
+
+      '@typescript-eslint/no-unused-vars': 'warn', // Variables no usadas solo avisan, no rompen
+      'react/no-unescaped-entities': 'off', // Permite escribir "don't" sin escapar comillas
+      '@next/next/no-img-element': 'off', // Permite usar <img> normal si no quieres usar <Image> siempre
     },
   },
-  prettierConfig, // Desactiva reglas de ESLint que entran en conflicto con Prettier
+  prettierConfig,
 
-  // eslint.config.mjs
   globalIgnores([
     '.next/**',
     'out/**',
