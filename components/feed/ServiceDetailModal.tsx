758db90f7a064e57202cededdb17939ac7e986fd
JuanSr02 <juanma2002123@gmail.com>
2026-02-12 07:25:16 -0300

    Ocultar en mobile

--- components/feed/ServiceDetailModal.tsx
+++ components/feed/ServiceDetailModal.tsx
@@ -271,7 +271,7 @@ const ServiceDetailModal: React.FC<Props> = ({
             </button>
           </div>
           {/* --- Formulario (prueba) --- */}
-          <div className="mt-10 border-t border-gray-100 pt-8 dark:border-gray-800">
+          <div className="hidden md:block mt-10 border-t border-gray-100 pt-8 dark:border-gray-800">
             <h3 className="mb-6 text-2xl font-bold text-gray-900 dark:text-white">
               Deja tu Reseña
             </h3>
@@ -296,9 +296,11 @@ const ServiceDetailModal: React.FC<Props> = ({
           </div>
         </div>
       </div>
-      <hr className="my-10 border-gray-100 dark:border-gray-800" />
+      <div className="hidden md:block">
+        <hr className="my-10 border-gray-100 dark:border-gray-800" />
 
-      <ReviewsSection servicioId={service.id} refreshKey={reviewsRefreshKey} />
+        <ReviewsSection servicioId={service.id} refreshKey={reviewsRefreshKey} />
+      </div>
     </div>
   );
 };
